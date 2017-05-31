4977619a63415c9db783c5e3480b9ffcce7448e8
Samuel Duzett <samuel.duzett@gmail.com>
2017-05-30 19:24:58 -0600

    remove extraneous ryan files

--- app/bishop.js
+++
@@ -1,8 +0,0 @@
-import Piece from './piece';
-
-export default class Bishop extends Piece {
-	constructor(row, col, color) {
-		super(row, col, color);
-		this.image = color === 'white' ? '2657' : '265D';
-	}
-}
--- app/king.js
+++
@@ -1,8 +0,0 @@
-import Piece from './piece';
-
-export default class King extends Piece {
-	constructor(row, col, color) {
-		super(row, col, color);
-		this.image = color === 'white' ? '2654' : '265A';
-	}
-}
